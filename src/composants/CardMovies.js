8ac5edb0f70b9d205c3cedec497887bcc0bdc81e
ZephirSD <siegfrieddallery@gmail.com>
2023-03-31 14:15:27 +0400

    Réalisation de la section de recherche et insertion des genres et des dates.

--- src/composants/CardMovies.js
+++ src/composants/CardMovies.js
@@ -1,12 +1,21 @@
-import React, {  } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const CardMovies = ({ movie }) => {
-    // const [dateMovie, setdateMovie] = useState();
-    // const dateFonction = () => {
-    //     let date = new Date(movie);
-    //     setdateMovie(date.toLocaleDateString("fr-FR"));
-    // }
-    // useEffect(dateFonction);
+    const [genresState, setgenresState] = useState([]);
+    const fetchGenres = () => {
+        axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=7e84c45fe73529dd9f3040600fd5802a&language=fr-FR`)
+            .then(response => setgenresState(response.data.genres));
+    }
+
+    function stringDateFR(dateValue) {
+        let stringDate;
+        let date = new Date(dateValue);
+        stringDate = date.toLocaleDateString("fr-FR");
+        return stringDate;
+    }
+
+    useEffect(fetchGenres, []);
     return (
         <>
            <div className="card">
@@ -14,12 +23,24 @@ const CardMovies = ({ movie }) => {
                 <div className="date_movies" style={{marginBottom: "5px"}}>
                     <span>Date:</span>
                     <span style={{ marginLeft: "5px" }}>
-                        {  }
+                        { stringDateFR(movie.release_date) }
                     </span>
                 </div>
                 <div className="origin_repas" style={{marginBottom: "5px"}}>
-                    <span>Nombre de votes:</span>
-                    <span style={{marginLeft: "5px"}}>{ movie.vote_count }</span>
+                    <span>Genres:</span>
+                    <span style={{ marginLeft: "5px" }}>
+                        {
+                            movie.genre_ids.map((genre) => (
+                                genresState.map((stGenre) => (
+                                    genre === stGenre.id ? stGenre.name : <></>
+                                ))
+                            ))
+                        }
+                    </span>
+                </div>
+                <div className="origin_repas" style={{ marginBottom: "5px" }}>
+                    <span>Note:</span>
+                    <span style={{marginLeft: "5px"}}>{ `${Math.round(movie.vote_average).toString()}/10` }</span>
                 </div>
                 <div className="img_movies">
                     <img src={`https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`} alt="img_movie" />
--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -5,6 +5,8 @@ import axios from 'axios';
 
 const Accueil = () => {
     const [filmList, setfilmList] = useState([]);
+    const [searchValue, setSearchValue] = useState("");
+    const [valueInput, setValueInput] = useState("");
     const fetchMovies = () => {
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=7e84c45fe73529dd9f3040600fd5802a&query=code&language=fr-FR`)
             .then(response => setfilmList(response.data.results));
@@ -12,7 +14,7 @@ const Accueil = () => {
     useEffect(fetchMovies, []);
     return (
         <>
-            <header>
+            <header style={{ position: "relative"}}>
                 <nav>
                     <ul>
                         <li><NavLink to={"/"}>Accueil</NavLink></li>
@@ -20,13 +22,18 @@ const Accueil = () => {
                     </ul>
                 </nav>
                 <div className="searchCase">
-                    <input type="text" placeholder='Chercher le film' />
-                    <button className="btnSearch">Rechercher</button>
+                    <input type="text" placeholder='Chercher le film' value={valueInput} onChange={(e) => setValueInput(e.target.value)} />
+                    <button className="btnSearch" onClick={(e) => setSearchValue(valueInput)}>Rechercher</button>
+                </div>
+                <div className="caseTopFlop" style={{ position: "absolute" }}>
+                    <button className="btnTopFlop">Top</button>
                 </div>
             </header>
             <section className="gridMovies">
                 {
-                    filmList.map((film, index) => (
+                    filmList && filmList
+                        .filter((search) => search.title.toLowerCase().includes(searchValue.toLowerCase()))
+                        .map((film, index) => (
                         <CardMovies key={index} movie={film}/>
                     ))
                 }
